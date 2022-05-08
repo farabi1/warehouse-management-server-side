a8c5d85ac92b1cbe8c901d6a43862f511f1374bf
farabi1 <rashidfarabi@gmail.com>
2022-05-08 22:45:44 +0600

    Server side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config();
 
@@ -28,7 +28,28 @@ async function run() {
       res.send(inventory);
     });
 
+    app.get('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const inventory = await inventoryCollection.findOne(query);
+      res.send(inventory);
+    })
+
+    app.post('/inventory', async (req, res) => {
+      const newInventory = req.body;
+      const result = await inventoryCollection.insertOne(newInventory);
+      res.send(result);
+    })
+
+    app.delete('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query={_id:ObjectId(id)};
+      const result=await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
+
   }
+
   finally {
 
   }
